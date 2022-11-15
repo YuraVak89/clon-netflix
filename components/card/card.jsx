17f86e16d5fe3114d99d5217a781c2ad088fac3f
Yura <1vakyura1@gmail.com>
2022-11-15 20:06:47 +0200

    KURS part signIn

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useState } from "react";
 import styles from "./card.module.css";
 import { motion } from "framer-motion";
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,13 +1,42 @@
 import styles from "./navbar.module.css";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import { magic } from "../../lib/magic-client";
 
-const Navbar = (props) => {
-  const { userName } = props;
+const Navbar = () => {
   const router = useRouter();
   const [showDropDown, setShowDropDown] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    async function getUsername() {
+      try {
+        const { email } = await magic.user.getMetadata();
+        if (email) {
+          console.log(email);
+          setUsername(email);
+        }
+      } catch (error) {
+        console.log("Error retrieving email:", error);
+      }
+    }
+    getUsername();
+  }, []);
+
+  const handleSignout = async (e) => {
+    e.preventDefault();
+
+    try {
+      await magic.user.logout();
+      console.log(await magic.user.isLoggedIn());
+      router.push("/signin");
+    } catch (error) {
+      console.error("Error logging out", error);
+      router.push("/signin");
+    }
+  };
 
   const handleOnClickHome = (e) => {
     e.preventDefault();
@@ -29,12 +58,7 @@ const Navbar = (props) => {
       <div className={styles.wrapper}>
         <a className={styles.logoLink}>
           <div className={styles.logoWrapper}>
-            <Image
-              src="/static/images/Netflix-Logo.png"
-              alt="Netflix-logo"
-              width="128"
-              height="34"
-            />
+            <Image src="/static/images/Netflix-Logo.png" alt="Netflix-logo" width="128" height="34" />
           </div>
         </a>
         <ul className={styles.navItems}>
@@ -47,24 +71,16 @@ const Navbar = (props) => {
         </ul>
         <nav className={styles.navContainer}>
           <div>
-            <button
-              className={styles.usernameBtn}
-              onClick={hsndlerShowDropDown}
-            >
-              <p className={styles.userName}>{userName}</p>
-              <Image
-                src="/static/images/expand_more.svg"
-                alt="Expand more"
-                width="24"
-                height="24"
-              />
+            <button className={styles.usernameBtn} onClick={hsndlerShowDropDown}>
+              <p className={styles.userName}>{username}</p>
+              <Image src="/static/images/expand_more.svg" alt="Expand more" width="24" height="24" />
             </button>
             {showDropDown && (
               <div className={styles.navDropdown}>
                 <div>
-                  <Link href="/login" className={styles.linkName}>
+                  <a className={styles.linkName} onClick={handleSignout}>
                     Sign out
-                  </Link>
+                  </a>
                   <div className={styles.lineWrapper}></div>
                 </div>
               </div>
